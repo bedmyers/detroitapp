283df923a1cbf87dbeb7a28cfa1d6d0272e69836
Blair Myers <blair.myers@wnco.com>
2023-05-30 22:20:17 -0400

    Initial Commit

---
+++ DetroitApp/DetroitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetroitAppApp.swift
+//  DetroitApp
+//
+//  Created by Blair Myers on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DetroitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
